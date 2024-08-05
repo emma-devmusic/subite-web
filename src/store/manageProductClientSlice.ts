d78b185b56c77686cfc5eff31d64b00c59cd0dcd
Emma Araujo <ema.araujo.ea@gmail.com>
2024-08-04 22:30:59 -0300

    setting product

--- src/store/manageProductClientSlice.ts
+++ src/store/manageProductClientSlice.ts
@@ -9,7 +9,9 @@ const manageUsersSlice = createSlice({
     name: 'manageProductClient',
     initialState,
     reducers: {
-       
+       searchProcuts(state, action: PayloadAction) {
+
+       },
     }
 });
 
--- src/store/middlewares/manage-auction-product-client-middleware.ts
+++ src/store/middlewares/manage-auction-product-client-middleware.ts
@@ -3,5 +3,11 @@ import { Dispatch, MiddlewareAPI } from "@reduxjs/toolkit";
 export const manageUserAuditsMiddleware = (state: MiddlewareAPI) => {
     return (next: Dispatch) => async (action: any) => {
         next(action);
+
+
+        
+
+
+
     }
 }
